54f67ce3f80eb3fbfe3e7f27d46a5d43da743b59
Abdul Majid <abdulmajidcse@gmail.com>
2022-08-15 21:01:51 +0600

    react setup with vite.

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+import "./bootstrap";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(
+    <>
+        <h1>Hello Mailbot!</h1>
+    </>
+);
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+Route::view('/{app}', 'app')->where('app', '.*');
